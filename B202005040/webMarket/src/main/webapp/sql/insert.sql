8a627f8ce60508186e9a3ae67e71b4bb62084b2e
jongsikLEE01 <sononot445@gmail.com>
2023-11-22 12:46:32 +0900

    23.11.22 테이블생성, 상품 DB에 추가

---
+++ B202005040/webMarket/src/main/webapp/sql/insert.sql
@@ -0,0 +1,8 @@
+insert into product values('P1234', 'iPhone 6s', 800000, '4.7-inch,1334X750 Renina HD display,8-megapixel iSight Camera', 
+'Smart Phone', 'Apple', 1000, 'new', 'P1234.png');
+insert into product values('P1235','LG PC그램',1500000,'13.3-inch,IPS LED display,5rd Generation Intel Core processors',
+'Notebook', 'LG', 1000, 'new', 'P1235.png');
+insert into product values('P1236','GalaxyTabS',900000,'3.3-inch, 212.8*125.6*6.6mm, Super AMOLED display, Octa-Core processor',
+'Tablet', 'Samsung', 1000, 'old', 'P1236.png');
+
+
